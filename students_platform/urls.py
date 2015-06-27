1bec74834dbf50c03e3c1126a8fff0c670b09ba9
Andrei Docea <corax.albus@gmail.com>
2015-06-27 12:35:44 +0300

    Changed the urls.py file to contain urls for 'All forums', 'All posts in a forum' and 'Details about a single post' pages

--- students_platform/urls.py
+++ students_platform/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.forum_list),
-	url(r'^$', views.post_list),
+    url(r'^forum/(?P<forum_name>+)/$', views.post_list),
+	url(r'^forum/(?P<forum_name>+)/post/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
