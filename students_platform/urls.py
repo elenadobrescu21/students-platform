291f9386e649fea7126036946ace31e2777710ab
Andrei Docea <corax.albus@gmail.com>
2015-06-27 17:12:06 +0300

    Moved the static folder where it belonged.

--- students_platform/urls.py
+++ students_platform/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.forum_list),
     url(r'^forum/(?P<forum_name>)/$', views.post_list),
 	url(r'^forum/(?P<forum_name>)/post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^forum/(?P<forum_name>)/post/new/$', views.post_new, name='post_new'),
+    url(r'^forum/(?P<forum_name>)/post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
